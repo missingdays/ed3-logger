7a2f57c5f1f20b0bd3d177a8c70066b25946ca56
Evgeny Bovykin <missingdays@etersoft.ru>
2015-08-21 11:45:17 +0300

    Change logger due to specifications

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
-var logger = {
-};
+define(function() {
+    var MAX_LOG_MESSAGE_LENGTH = 300;
 
-var shouldLog = true
+    var Logger = function(moduleName, suffix) {
+      this.module = moduleName;
+      this.suffix = suffix;
+    };
 
-logger.log = function(){
-    if(shouldLog){
-        console.log.apply(console, Array.prototype.slice.call(arguments));
-    }
-};
+    var trimMessage = function(msg) {
+      msg = msg === 0 ? '0' : (msg || '').toString();
+      return msg.length <= MAX_LOG_MESSAGE_LENGTH ? msg :
+      (msg.substr(0, MAX_LOG_MESSAGE_LENGTH) + ' ... (message trimmed; ' + msg.length + ' symbols total)');
+    };
 
-logger.setLog = function(){
-    shouldLog = true;
-};
+    Logger.prototype = {
+      _processMsg: function(msg) {
+        return '[' + this.module + (this.suffix ? ':' + this.suffix : '') + ']: ' + trimMessage(msg);
+      },
+      error: function (msg) {
+        console.error(this._processMsg(msg));
+      },
+      warn: function (msg) {
+        console.warn(this._processMsg(msg));
+      },
+      info: function (msg) {
+        console.info(this._processMsg(msg));
+      },
+      debug: function (msg) {
+        // node compability
+        if(console.debug){
+            console.debug(this._processMsg(msg));
+        } else {
+            console.info(this._processMsg(msg));
+        }
+      },
+    };
 
-logger.unsetLog = function(){
-    shouldLog = false;
-};
+    var loggers = {}, 
+        defaultLogger = new Logger('default');
 
-if (typeof define === 'function' && define.amd) {
-    define("ed3-logger", logger);
-} else if ('undefined' !== typeof exports && 'undefined' !== typeof module) {
-    module.exports = logger;
-}
-
-
-
+    return {
+      getLogger: function (module, suffix) {
+        if (!module) {
+          return defaultLogger;
+        }
+        var logger = loggers[module + suffix];
+        if (!logger) {
+          logger = new Logger(module, suffix);
+          loggers[module + suffix] = logger;
+        }
+        return logger;
+      }
+    };
+});
---
+++ test.js
@@ -0,0 +1,7 @@
+var requirejs = require("requirejs");
+var logger = requirejs("./index").getLogger("main");
+
+logger.info("info");
+logger.debug("debug");
+logger.error("error");
+logger.warn("warn");
